f0fcd4b8a97d719e5611f85ae4595587b7531907
Harsh <hvgurjar1@gmail.com>
2024-08-18 21:07:46 +0530

    Setup Crud with Final Commit

---
+++ EmployeeAdminPortal/Models/UpdateEmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeAdminPortal.Models
+{
+    public class UpdateEmployeeDto
+    {
+        public string? Phone { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
