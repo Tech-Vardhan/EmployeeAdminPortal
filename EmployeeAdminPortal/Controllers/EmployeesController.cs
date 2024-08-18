eab254619e496e25dabacb62e443f4fc94bfd1f9
Harsh <hvgurjar1@gmail.com>
2024-08-18 20:32:15 +0530

    Setup ConnectString,Migration Database

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(_context.Employees.ToList());
+        }
+    }
+}
